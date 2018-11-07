c76dcac726c2eeefa67e643ea532e34df1341d83
GuillermoGC <mrgonzalezcruz@gmail.com>
2018-11-07 08:56:22 -0600

    Estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,19 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModuleModule } from './/material-module.module';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainNavComponent } from './core/sidenav_toolbar/main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
-import { ExampleNavComponent } from './example-nav/example-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    ExampleNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModuleModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
